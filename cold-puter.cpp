e294bf9868fd2b73f869ed1abbfccd4fbd211bc5
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2022-09-10 10:58:51 +0700

    Add files via upload

---
+++ cold-puter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Deklarasi Variable
+    int n, t;
+    int hasil = 0;
+
+    // minta inputan dari user
+    cin >> n;
+
+    // input suhu dengan perulangan
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> t;
+
+        // kalo suhu yang diinputkan dibawah 0 derajat, hasil + 1
+        if (t < 0)
+        {
+            hasil++;
+        }
+    }
+
+    cout << hasil;
+
+    return 0;
+}
