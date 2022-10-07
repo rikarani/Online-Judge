492ee6f583805f878586150dfbfeaa58eacd18c2
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2022-10-07 16:55:34 +0700

    Add files via upload

---
+++ r2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // deklarasi variable
+    int r1;
+    int s;
+
+    // minta inputan dari user
+    cin >> r1 >> s;
+
+    // hitung
+    int r2 = (s * 2) - r1;
+
+    cout << r2 << endl;
+
+    return 0;
+}
