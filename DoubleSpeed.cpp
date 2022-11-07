b13fe0af19d01005176f11ec3a84ed82b3cdd1cc
rikarani <phasyaananta1502@gmail.com>
2022-11-07 13:11:15 +0700

    add file

---
+++ DoubleSpeed.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // deklarasi variable
+    double jarak, waktu; // jarak dalam meter, waktu dalam menit
+    double kecepatan = 0;
+
+    // minta inputan dari user
+    cin >> jarak;
+    cin >> waktu;
+
+    kecepatan = ((jarak / 1000) / (waktu / 60)) * 2;
+
+    cout << kecepatan << " km/jam";
+
+    return 0;
+}
